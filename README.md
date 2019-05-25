987fb22e357615278aaac0ead1db4960ffa351b4
Colin Van Oort <omega1563@gmail.com>
2019-05-25 18:17:20 -0400

    Added an additonal implementation of the radial coordinate function that operates on numpy arrays.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Hyperbolic Coaelesccent Embeddings in Python
 
-A Python implementation of "Machine learning meets complex networks viacoalescent embedding in the hyperbolic space".
+A Python implementation of "Machine learning meets complex networks via coalescent embedding in the hyperbolic space".
 
 Matlab version by the authors: https://github.com/biomedical-cybernetics/coalescent_embedding
--- radial_coord.py
+++ radial_coord.py
@@ -1,17 +1,47 @@
 import math
+import numpy as np
 
-def radial_coord(G, zeta, beta):
+
+def radial_coord(G, beta, zeta=1):
     """
-        Nodes are first sorted i = 1,2, ... N according to their degree
-        (descending).
+    Nodes are first sorted i = 1,2, ... N according to their degree
+    (descending).
 
-        Then r_i is calculated by the formula in pg. 3
+    Then r_i is calculated by the formula in pg. 3
 
-        r_i = \frac{2}{\zeta} ( \beta ln i + (1 - \beta) ln N )
+    r_i = \frac{2}{\zeta} ( \beta ln i + (1 - \beta) ln N )
+
+    Args:
+        G: networkx.Graph
+        zeta: float, sqrt(-K), where K is the curature of the hyperbolic space.
+        beta: float, Exponent of the power-law degree distribution.
+
+    Returns:
+
     """
-    sorted_node_ids = [i for i, k in sorted(G.degree, key=lambda x: x[1], reverse=True)]
+    sorted_node_ids = [i for i, _ in sorted(G.degree(), key=lambda x: x[1], reverse=True)]
 
-    for i, n in sorted_node_ids:
-        r = 2/zeta * ( beta * math.log(i) + (1 - beta) math.log(len(G)))
-        yield i, r
+    for i, n in enumerate(sorted_node_ids):
+        r = 2 / zeta * (beta * math.log(i) + (1 - beta) * math.log(len(G)))
+        yield n, r
 
+
+def radial_coord_deg(degrees, beta, zeta=1):
+    """
+    Nodes are first sorted i = 1,2, ... N according to their degree
+    (descending).
+
+    Then r_i is calculated by the formula in pg. 3
+
+    r_i = \frac{2}{\zeta} ( \beta ln i + (1 - \beta) ln N )
+
+    Args:
+        degrees: numpy.ndarray, Degree of each node in some graph.
+        beta: float, Exponent of the power-law degree distribution.
+        zeta: float, sqrt(-K), where K is the curature of the hyperbolic space.
+
+    Returns: numpy.ndarray
+        Radial coordinate for each node in a graph.
+    """
+    inds = np.argsort(degrees)
+    return 2 / zeta * (beta * np.log(inds) + (1 - beta) * np.log(len(inds)))
