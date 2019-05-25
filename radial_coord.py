1e689fea406a3716d5ec61729df2134fc7334b0c
YY Ahn <yongyeol@gmail.com>
2019-05-25 16:58:33 -0400

    docstring

--- radial_coord.py
+++ radial_coord.py
@@ -1,2 +1,10 @@
 def radial_coord(G, zeta, beta):
+    """
+        Nodes are first sorted i = 1,2, ... N according to their degree
+        (descending).
+
+        Then r_i is calculated by the formula in pg. 3
+
+        r_i = \frac{2}{\zeta} ( \beta ln i + (1 - \beta) ln N )
+    """
     pass
