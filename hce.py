23bfbc3b62d4ea3f5f7937c0375f317757d32f00
YY Ahn <yongyeol@gmail.com>
2019-05-26 09:30:16 -0400

    a bug fix

--- hce.py
+++ hce.py
@@ -29,7 +29,7 @@ def radial_coord(G, zeta, beta):
     sorted_node_ids = [i for i, k in sorted(G.degree, key=lambda x: x[1], reverse=True)]
 
     for i, n in sorted_node_ids:
-        r = 2/zeta * ( beta * math.log(i) + (1 - beta) math.log(len(G)))
+        r = 2/zeta * ( beta * math.log(i) + (1 - beta) * math.log(len(G)))
         yield i, r
 
 
